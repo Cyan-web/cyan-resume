11b586abd7c5bd416b3187e8f75e58b620b6d982
Cyan <869071331@qq.com>
2022-05-18 12:37:36 +0800

    额诗琳通、

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld.vue'
 
-const a = ref('1');
+const a = ref('1')
 </script>
 
 <template>
@@ -11,12 +11,4 @@ const a = ref('1');
 </template>
 
 <style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
 </style>
